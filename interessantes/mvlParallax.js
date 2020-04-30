44bcd75c5c716bfac111c9570f2c3a6b56c083a9
Renan Machado <renan@rdmachado.com.br>
2020-04-30 14:22:16 -0300

    Update mvlParallax.js

--- interessantes/mvlParallax.js
+++ interessantes/mvlParallax.js
@@ -17,44 +17,57 @@
     }
 
     function mvl_parallax(el,speed){
-        // console.log(el.css('transform'));
-        if(el.css('matrix')==undefined) {
-            el.css({
-                transition: ".01s",
-                '-webkit-transform': "translateY( 0,1px )",
-                '-moz-transform': "translateY( 0,1px )",
-                'transform': "translateY( 0,1px )",
-                'will-change': 'transform'
-            });
-        }
-            
-        var windowHeight = $(window).height();
-        var elScroll = el.offset().top;
-        var windowCenter = windowHeight/2;
+      // console.log(el.css('transform'));
+      // if(el.css('matrix')==undefined) {
+      //     el.css({
+      //         transition: ".0s ease-out",
+      //         '-webkit-transform': "translateY( 0,1px )",
+      //         '-moz-transform': "translateY( 0,1px )",
+      //         'transform': "translateY( 0,1px )",
+      //         'will-change': 'transform',
+      //     });
+      // }
+          
+      var windowHeight = $(window).height();
+      var elScroll = el.offset().top;
+      var windowCenter = windowHeight/2;
 
-        // Verifica posição da tela
-        var elTop = elScroll-windowHeight;
-        var elBottom = elScroll+windowHeight;
+      // Verifica posição da tela
+      var elTop = elScroll-windowHeight;
+      var elBottom = elScroll+windowHeight;
 
-        // console.log(el,windowHeight,elTop,elBottom);
+      // console.log(el,windowHeight,elTop,elBottom);
 
-        $(window).scroll(function(e){
-          // Get Scroll
-          var currentScroll = $(this).scrollTop();
-          var wheel = ( ( (currentScroll-elScroll )-( el.height()/2 )+windowCenter )/speed );
-                     
-            if($(document).scrollTop()>elTop && $(document).scrollTop()<elBottom){
+      function mvl_parallax_scroll(e){
+        // Get Scroll
+        var currentScroll = $(this).scrollTop();
+        var wheel = ( ( (currentScroll-elScroll )-( el.height()/2 )+windowCenter )/speed );
 
-                // console.log(el,wheel);
+          el.css({
+              '-webkit-transform': "translateY( "+wheel*-1+"%)",
+              '-moz-transform': "translateY( "+wheel*-1+"%)",
+              'transform': "translateY( "+wheel*-1+"%)",
+          });
+                   
+          if($(document).scrollTop()>elTop && $(document).scrollTop()<elBottom){
 
-                el.css({
-                    '-webkit-transform': "translateY( "+wheel*-1+"%)",
-                    '-moz-transform': "translateY( "+wheel*-1+"%)",
-                    'transform': "translateY( "+wheel*-1+"%)",
-                });
-            }
-        });
-    }
+              // console.log(el,wheel);
+
+              el.css({
+                transition: "1s ease-out",
+                  '-webkit-transform': "translateY( "+wheel*-1+"%)",
+                  '-moz-transform': "translateY( "+wheel*-1+"%)",
+                  'transform': "translateY( "+wheel*-1+"%)",
+              });
+          }
+      }
+      mvl_parallax_scroll();
+
+
+      $(window).scroll(function(e){
+        mvl_parallax_scroll(e);
+      });
+  }
     mvl_parallax_top($('.slider-content-image'),0.05);
     mvl_parallax($('.sobre_imagem'),20);
     mvl_parallax($('.service_resumo'),20);
